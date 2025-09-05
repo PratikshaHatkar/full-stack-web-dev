3ca9b2eb4b8ce1b97ae53c4fb334316bfe368ac4
PratikshaHatkar <149888808+PratikshaHatkar@users.noreply.github.com>
2025-09-06 00:53:25 +0530

    Delete video  82 Advance Javascript directory

--- video  82 Advance Javascript/script.js
+++
@@ -1,9 +0,0 @@
-async function sleep(){
-    return new Promise((resolve , reject)=>{
-        setTimeout(() => {
-            resolve(45)
-        }, 1000);
-    })
-}
-let a = await sleep()
-let b = await sleep()
